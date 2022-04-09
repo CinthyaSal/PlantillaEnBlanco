138d8d765a5dafbebd026029daeae75f6f1a834e
CinthyaSal <83619912+CinthyaSal@users.noreply.github.com>
2022-04-09 11:28:24 -0500

    Add files via upload

---
+++ Plantilla/sketch.js
@@ -0,0 +1,17 @@
+//Variables Globales
+
+function preload(){
+  //precargar imagenes y sonidos
+  
+}
+
+function setup(){
+ //Crear Sprites
+  createCanvas(600,600);
+  
+}
+
+function draw(){
+ 
+ 
+}
